6305a579448cc68566dcd57b0bffc0ca09911fc3
Igor Ferreira <igoreira0@gmail.com>
2022-10-27 23:38:31 -0300

    created singletons for mongo and redis

--- api/blueprints/tests.py
+++ api/blueprints/tests.py
@@ -1,17 +1,18 @@
 from flask import Blueprint
 import redis
-import pymongo
-from api.utils.constants import MONGO_URI, MONGODB_DATABASE, MONGODB_TESTS_COLLECTION, REDIS_HOST, REDIS_PASS, REDIS_PORT, REDIS_TEST_DB, REDIS_USER
+from api.utils.constants import MONGODB_TESTS_COLLECTION, REDIS_TEST_DB
 from api.utils.custom_response import SUCCESS_CODE, custom_response
+from api.utils.mongo_adapter_factory import create_database_connection
+from api.utils.queue_factory import create_queue_connection
 
 test_api = Blueprint('test_api', __name__)
 
-mongo = pymongo.MongoClient(MONGO_URI)[MONGODB_DATABASE][MONGODB_TESTS_COLLECTION]
+redis = create_queue_connection(REDIS_TEST_DB)
+mongo = create_database_connection(MONGODB_TESTS_COLLECTION)
 
 @test_api.route('/<test_id>', methods=['GET', 'POST'])
 def exec_test(test_id):
-    with redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_TEST_DB, ssl=True, username=REDIS_USER, password=REDIS_PASS) as redis_client:
-        redis_client.lpush('test', test_id)
+    redis.lpush('test', test_id)
     return custom_response({'status': f'Test {test_id} inserted on the queue.'}, SUCCESS_CODE)
 
 @test_api.route('/report/<test_id>', methods=['GET'])
